22fa8b9c574461c99ff2938df142f95a80e2b115
Ayush Tripathy <ayushtripathy07@gmail.com>
2026-05-15 12:36:32 +0530

    Implement number printing function
    Add a print function that outputs numbers from 1 to n without spaces.

---
+++ Day03/Print function.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    # Iterate from 1 up to (and including) n
+    for i in range(1, n + 1):
+        # Print the current number without a space or newline
+        print(i, end="")
