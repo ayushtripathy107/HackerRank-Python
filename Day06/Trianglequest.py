8baccfeaf282a4be2af28ce2f8dc681d56e6dde6
Ayush Tripathy <ayushtripathy07@gmail.com>
2026-05-20 14:10:59 +0530

    Add triangle number pattern generator in Trianglequest.py
    Implement a triangle number pattern generator.

---
+++ Day06/Trianglequest.py
@@ -0,0 +1,11 @@
+for i in range(1, int(input()) + 1): # More than 2 lines will result in 0 score.
+    print(((10**i - 1) // 9)**2)
+
+#Sample Input
+#5
+#Sample Output
+#1
+#121
+#12321
+#1234321
+#123454321
