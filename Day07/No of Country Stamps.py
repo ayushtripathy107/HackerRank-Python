fad706fe00c3c6d5c955c4aa6e2a2d0642b4ae1e
Ayush Tripathy <ayushtripathy07@gmail.com>
2026-05-21 12:39:07 +0530

    Add script to count unique country stamps

---
+++ Day07/No of Country Stamps.py
@@ -0,0 +1,13 @@
+# Read the total number of stamps
+n = int(input())
+
+# Initialize an empty set to store unique country names
+country_set = set()
+
+# Loop N times to read each country name and add it to the set
+for _ in range(n):
+    country_name = input().strip()
+    country_set.add(country_name)
+
+# The size of the set represents the number of distinct stamps
+print(len(country_set))
